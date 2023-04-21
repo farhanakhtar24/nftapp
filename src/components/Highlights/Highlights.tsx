a7989de7225a4455d0cff65ed583b1772ab08e69
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2023-04-21 17:54:25 +0530

    done

--- src/components/Highlights/Highlights.tsx
+++ src/components/Highlights/Highlights.tsx
@@ -4,10 +4,10 @@ import HighlightCard from "./HighlightCard";
 
 // Import the images used for the highlight cards
 import ENSPhoto from "../../../public/highlightImages/ENS photo svg.svg";
-import BestFlip from "../../../public/assets/highlightImages/Best Flip.svg";
-import FirstNFTBought from "../../../public/assets/highlightImages/First NFT Bought.svg";
-import PaperHanded from "../../../public/assets/highlightImages/Paper Handed.svg";
-import ShouldHaveSold from "../../../public/assets/highlightImages/Should Have Sold.svg";
+import BestFlip from "../../../public/highlightImages/Best Flip.svg";
+import FirstNFTBought from "../../../public/highlightImages/First NFT Bought.svg";
+import PaperHanded from "../../../public/highlightImages/Paper Handed.svg";
+import ShouldHaveSold from "../../../public/highlightImages/Should Have Sold.svg";
 
 // Define the type for the props
 type Props = {};
